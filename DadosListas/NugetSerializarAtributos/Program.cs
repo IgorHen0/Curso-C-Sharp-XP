737ad013834f956b21a57702d1d1b11b99327e27
IgorHen0 <igorhenrique0472@gmail.com>
2024-10-25 23:42:48 -0300

    nuget, serializar e atributos

---
+++ DadosListas/NugetSerializarAtributos/Program.cs
@@ -0,0 +1,23 @@
+﻿using NugetSerializarAtributos.Models;
+using Newtonsoft.Json;
+
+List<Venda> listaVendas = new List<Venda>();
+
+DateTime dataAtual = DateTime.Now;
+
+Venda venda1 = new Venda(1, "Caneta", 1.99m, dataAtual);
+Venda venda2 = new Venda(2, "Lápis", 0.99m, dataAtual);
+Venda venda3 = new Venda(3, "Borracha", 0.50m, dataAtual);
+Venda venda4 = new Venda(4, "Apontador", 1.50m, dataAtual);
+
+listaVendas.Add(venda1);
+listaVendas.Add(venda2);
+listaVendas.Add(venda3);
+listaVendas.Add(venda4);
+
+// string serializado = JsonConvert.SerializeObject(new Venda[] { venda1, venda2, venda3, venda4 }, Formatting.Indented);
+string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
+
+File.WriteAllText("Arquivos/vendas.json", serializado);
+
+Console.WriteLine(serializado);
